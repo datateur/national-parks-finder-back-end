d3e308779bde2b63c92db68b70e212c398e7798d
Jennifer <datateur@gmail.com>
2023-02-03 14:48:31 -0800

    debugging: moved cors init to bottom of app

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,6 @@ def create_app(test_config=None):
 
     app = Flask(__name__)
     app.config['DEBUG'] = True
-    CORS(app)
-
 
     # Register Blueprints here
     from .routes import parks_bp, activities_bp, topics_bp
@@ -15,4 +13,5 @@ def create_app(test_config=None):
     app.register_blueprint(activities_bp)
     app.register_blueprint(topics_bp)
 
+    CORS(app)
     return app
