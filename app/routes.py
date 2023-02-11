4287a11c756ad5f688fb1fb594c2fbca8b49f28b
Jennifer <datateur@gmail.com>
2023-02-11 15:55:28 -0800

    changed filter route name

--- app/routes.py
+++ app/routes.py
@@ -10,78 +10,7 @@ topics_bp = Blueprint("topics", __name__, url_prefix="/topics")
 
 NATIONAL_PARKS_SERVICE_API_KEY = os.environ['NATIONAL_PARKS_SERVICE_API_KEY']
 
-# returns json object of all the parks
-# @parks_bp.route('', methods=["GET"])
-# def get_all_parks():
-#     parks = Park.query.all()
-#     response = []
-#     for park in parks:
-#         response.append(park.to_dict())
-    
-#     return jsonify(response), 200
-
-
-# @parks_bp.route('/filter', methods=["POST"])
-# def get_parks_filtered_by_activity_and_topic():
-#     filter_activities = request.get_json()['activities']
-#     filter_topics = request.get_json()['topics']
-#     parks_by_activity = []
-#     parks_by_topic = []
-#     filtered_parks = []
-
-#     if not filter_activities and not filter_topics:
-#         for park in all_national_parks:
-#             filtered_parks.append({"park_id":park['parkCode'],
-#                                     "full_name":park['fullName'], 
-#                                     'description': park['description'],
-#                                     'latitude': float(park['latitude']) if park['latitude'] else None,
-#                                     'longitude': float(park['longitude']) if park['longitude'] else None,
-#                                     'states': [park['states']],
-#                                     'contacts': [park['contacts']],
-#                                     'entranceFees': [park['entranceFees']],
-#                                     'hours': park['operatingHours'],
-#                                     'designation': park['designation']})
-        
-#     else:
-#         for park in all_national_parks:
-#             if filter_activities:
-#                 for activity in filter_activities:
-#                     for park_activity in park['activities']:
-#                         if activity == park_activity['name'] and park not in parks_by_activity:
-#                             parks_by_activity.append({"park_id":park['parkCode'],
-#                                     "full_name":park['fullName'],
-#                                     'description': park['description'],
-#                                     'latitude': float(park['latitude']) if park['latitude'] else None,
-#                                     'longitude': float(park['longitude']) if park['longitude'] else None,
-#                                     'states': [park['states']],
-#                                     'contacts': [park['contacts']],
-#                                     'entranceFees': [park['entranceFees']],
-#                                     'hours': park['operatingHours'],
-#                                     'designation': park['designation']})
-            
-#             if filter_topics:
-#                 for topic in filter_topics:
-#                     for park_topic in park['topics']:
-#                         if topic == park_topic['name'] and park not in parks_by_topic:
-#                             parks_by_topic.append({"park_id":park['parkCode'],
-#                                     "full_name": park['fullName'],
-#                                     'description': park['description'],
-#                                     'latitude': float(park['latitude']) if park['latitude'] else None,
-#                                     'longitude': float(park['longitude']) if park['longitude'] else None,
-#                                     'states': [park['states']],
-#                                     'contacts': [park['contacts']],
-#                                     'entranceFees': [park['entranceFees']],
-#                                     'hours': park['operatingHours'],
-#                                     'designation': park['designation']})
-    
-#         if parks_by_activity and parks_by_topic:
-#             filtered_parks = [park for park in parks_by_activity if park in parks_by_topic]
-#         else:
-#             filtered_parks = parks_by_activity + parks_by_topic
-
-#     return jsonify(filtered_parks), 200
-
-@parks_bp.route('/filter/db', methods=["POST"])
+@parks_bp.route('/filter', methods=["POST"])
 def get_parks_filtered_db():
     filter_activities = request.get_json()['activities']
     filter_topics = request.get_json()['topics']
